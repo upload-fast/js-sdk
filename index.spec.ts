d8d74b44bf040df85838c6497d68f5dfacf0aacb
iyifr <adekileiyimide@gmail.com>
2024-08-11 12:24:11 +0100

    feat: added delete method

--- index.spec.ts
+++ index.spec.ts
@@ -27,20 +27,17 @@ describe('JavaScript SDK', () => {
 			lastModified: Date.now(),
 		})
 
-		const response = await uploadfast.upload({ file })
-		// expect(response.status).toBe(201)
+		const response = await uploadfast.upload({ file: file })
 		expect(response[0]?.file_name).toBe('spicymohitas.png')
 
-		// Capture the URL of the uploaded file for later use
-		// uploadedFileUrl = response?.data?.url as string
+		uploadedFileUrl = response[0]?.url!
 	})
 
-	// it('should delete the uploaded file and return valid response', async () => {
-	// 	// Use the captured URL of the uploaded file
-	// 	const response = await uploadfast.delete(uploadedFileUrl)
-
-	// 	expect(response.success).toBe(true)
-	// 	expect(response.status).toBe(200)
-	// 	expect(response.data.message).toContain('deleted successfully')
-	// })
+	it('should delete the uploaded file and return valid response', async () => {
+		// Use the captured URL of the uploaded file
+		const response = await uploadfast.delete({
+			resource: 'https://assets.uploadfast.dev/spicymohitas.png',
+		})
+		expect(response?.message).toBeTruthy()
+	})
 })
--- index.ts
+++ index.ts
@@ -12,13 +12,52 @@ export class UploadFast {
 		this.apiKey = apiKey
 	}
 
-	public async upload({ file }: { file: File }): ReturnType<() => Promise<UploadResponse>> {
+	/**
+	 * Uploads a file to the UploadFast service.
+	 *
+	 * @param {Object} options - The options object.
+	 * @param {File} options.file - The file to be uploaded.
+	 * @param {File | File[]} options.resource - The file(s) to be uploaded.
+	 * @returns {Promise<UploadResponse>} - A Promise that resolves to the upload response.
+	 * @throws {UF_Error} - Throws a `UF_Error` with status code 404 if the `file` parameter is not provided.
+	 * @throws {Error} - Throws an error if there is an issue with the file upload process.
+	 *
+	 * @example
+	 * const uploadFast = createClient({ apiKey: 'your-api-key' });
+	 *
+	 * try {
+	 *   const uploadResponse = await uploadFast.upload({ file: someFile });
+	 *   console.log(uploadResponse);
+	 * } catch (error) {
+	 *   console.error(error);
+	 * }
+	 */
+
+	public async upload({
+		file,
+		resource,
+	}: {
+		file: File
+		resource?: File[] | undefined
+	}): Promise<UploadResponse> {
 		if (!file) {
-			throw new UF_Error(404)
+			if (!resource) {
+				throw new UF_Error(404)
+			}
 		}
+
 		try {
 			const formData = new FormData()
-			formData.append('file', file)
+
+			if (file instanceof File) {
+				formData.append('file', file)
+			}
+
+			if (!file && Array.isArray(resource)) {
+				for (const item of resource) {
+					formData.append('file', item)
+				}
+			}
 
 			const requestOptions = {
 				method: 'POST',
@@ -29,7 +68,7 @@ export class UploadFast {
 			}
 
 			const response = await fetch(this.serverUrl + '/upload', requestOptions)
-			const uploadResponse = await response.json()
+			const uploadResponse: UploadResponse = await response.json()
 
 			if (!response.ok) {
 				throw new Error(`HTTP error! status: ${response.status}`)
@@ -41,6 +80,12 @@ export class UploadFast {
 		}
 	}
 
+	/**
+	 * Upload many images at once
+	 * @deprecated
+	 * @param {Object}
+	 * @returns
+	 */
 	public async uploadMany({ files }: { files: File[] }) {
 		if (!files) {
 			throw new UF_Error(404)
@@ -71,8 +116,76 @@ export class UploadFast {
 			throw new Error(`An error occurred during file upload: ${error}`)
 		}
 	}
+
+	public async delete({ resource }: { resource: string | string[] }) {
+		if (!resource) {
+			throw new Error('Missing resource to delete')
+		}
+
+		let deleteResponse: { message: 'File deleted successfully' } | undefined = undefined
+		try {
+			if (resource instanceof Array) {
+				for await (const item of resource) {
+					const requestOptions = {
+						method: 'DELETE',
+						body: JSON.stringify({
+							file_url: item,
+						}),
+						headers: {
+							'api-key': this.apiKey,
+						},
+					}
+
+					const response = await fetch(this.serverUrl + '/delete', requestOptions)
+					deleteResponse = await response.json()
+
+					if (!response.ok) {
+						throw new Error(
+							`An error occured while deleting the file! status: ${response.statusText}`
+						)
+					}
+				}
+			} else {
+				const requestOptions = {
+					method: 'DELETE',
+					body: JSON.stringify({
+						file_url: resource,
+					}),
+					headers: {
+						'api-key': this.apiKey,
+						'Content-Type': 'application/json',
+					},
+				}
+
+				const response = await fetch(this.serverUrl + '/delete', requestOptions)
+
+				if (!response.ok) {
+					throw new Error(
+						`An error occured while deleting the file! status: ${await response.text()}`
+					)
+				}
+
+				deleteResponse = await response.json()
+			}
+
+			return deleteResponse
+		} catch (error) {
+			throw new Error(`An error occurred during file delete: ${error}`)
+		}
+	}
 }
 
-export const createClient = ({ apiKey }: { apiKey: string }) => {
+/**
+ * Creates an instance of the {@link UploadFast} class.
+ *
+ * @param {Object} options - The options object.
+ * @param {string} options.apiKey - The API key required to use the UploadFast service.
+ * @returns {UploadFast} - An instance of the UploadFast class.
+ * @throws {Error} - Throws an error if the `apiKey` is not provided or is invalid.
+ *
+ * @example
+ * const uploadFast = createClient({ apiKey: 'your-api-key' });
+ */
+export const createClient = ({ apiKey }: { apiKey: string }): UploadFast => {
 	return new UploadFast({ apiKey })
 }
